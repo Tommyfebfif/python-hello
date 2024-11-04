65f7d701a68df1358f09797efef82fa818c5f8b9
Tommyfebfif <tomottyenjoy@gmail.com>
2024-11-05 07:11:23 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,11 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
-from pyramid.view import view_config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import logging
 import os
 
-@view_config(route_name='connectingPG', renderer='json')
 def connectingPG(request):
     try:
         PGengine = create_engine('postgresql://user:password@host:port/database')
@@ -21,6 +19,12 @@ def connectingPG(request):
         logging.info("DB Error.")
 
 def hello_world(request):
+    PGengine = create_engine('postgresql://user:password@host:port/database')
+    pgSession = sessionmaker(bind=PGengine)
+    session = pgSession()
+    fetresult = session.execute('SELECT * FROM your_table').fetchall()
+    session.close()
+    
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
@@ -36,8 +40,6 @@ if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
-        config.add_route('connectingPG', '/pg')
-        config.add_view(connectingPG, route_name='connectingPG')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
