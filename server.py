5834f292036d7aab6f29f2cdd3a973521383e4d0
Tommyfebfif <tomottyenjoy@gmail.com>
2024-11-04 20:35:11 +0900

    Update server.py

--- server.py
+++ server.py
@@ -3,13 +3,27 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from logging import getLogger
 import os
+
+def connectingPG():
+    try:
+        PGengine = create_engine('postgresql://user:password@host:port/database')
+        pgSession = sessionmaker(bind=PGengine)
+        session = pgSession()
+        fetresult = session.execute('SELECT * FROM your_table').fetchall()
+        session.close()
+    except Exception as e:
+        logging.exception(e)
 
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
     message = "Hello, " + name + "!\n"
+
+    connectingPG()
+    
     return Response(message)
 
 if __name__ == '__main__':
