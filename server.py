87103184b9e06cd5bf0948802559af63c3b583c9
Tommyfebfif <tomottyenjoy@gmail.com>
2024-11-05 21:39:22 +0900

    Update server.py

--- server.py
+++ server.py
@@ -6,40 +6,34 @@ from sqlalchemy.orm import sessionmaker
 import logging
 import os
 
-def connectingPG(request):
-    try:
-        PGengine = create_engine('postgresql://user:password@host:port/database')
-        pgSession = sessionmaker(bind=PGengine)
-        session = pgSession()
-        fetresult = session.execute('SELECT * FROM your_table').fetchall()
-        session.close()
-        # return fetresult
-        return Response("aaa")
-    except Exception as e:
-        logging.info("DB Error.")
+def home_view(request):
+    engine = create_engine(request.registry.settings['sqlalchemy.url'])
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.execute("SELECT * FROM your_table")
+    rows = result.fetchall()
+
+    html = "<html><body>"
+    for row in rows:
+        html += f"<p>{row}</p>"
+    html += "</body></html>"
+    return Response(html)
 
 def hello_world(request):
-    # PGengine = create_engine('postgresql://user:password@host:port/database')
-    # pgSession = sessionmaker(bind=PGengine)
-    # session = pgSession()
-    # fetresult = session.execute('SELECT * FROM your_table').fetchall()
-    # session.close()
-    
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
     message = "Hello, " + name + "!\n"
-
-    # pgdata = connectingPG()
-    # logging.info(pgdata)
-    
     return Response(message)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
+#        config.add_route('hello', '/')
+#        config.add_view(hello_world, route_name='hello')
+        config.add_route('home', '/')
+        config.add_view(home_view, route_name='home')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
